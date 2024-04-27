34246327e772f5491abb9389665612557da78ef2
Pooja Dissanayake <poojadissanayake6@gmail.com>
2024-04-27 16:04:14 +1000

    Many robots

--- Robot.cs
+++ Robot.cs
@@ -5,6 +5,7 @@ public class Robot
     public double X { get; private set; }
     public double Y { get; private set; }
     public Color MainColor { get; private set; }
+    public Vector2D Velocity { get; private set; }
     public int Width
     {
         get { return 50; }
@@ -19,11 +20,76 @@ public class Robot
         get; private set;
     }
 
-    public Robot(Window gameWindow)
+    public Robot(Window gameWindow, Player player)
     {
-        X = SplashKit.Rnd(gameWindow.Width - Width);
-        Y = SplashKit.Rnd(gameWindow.Height - Height);
         MainColor = Color.RandomRGB(200);
+
+        const int speed = 4;
+
+        // get a point for the robot
+        Point2D fromPt = new Point2D() { X = X, Y = Y };
+
+        // get a point for the player
+        Point2D toPt = new Point2D()
+        {
+            X = player.X,
+            Y = player.Y
+        };
+
+        //calculate the direction to head
+        Vector2D dir;
+        dir = SplashKit.UnitVector(SplashKit.VectorPointToPoint(fromPt, toPt));
+
+        // randomly pick top/bottomor left/right
+        if (SplashKit.Rnd() < 0.5)
+        {
+
+            //  top or bottom
+            //  start by picking a random position left or right (X)
+            X = SplashKit.Rnd(gameWindow.Width);
+
+            // work out if top or bottom?
+            if (SplashKit.Rnd() < 0.5)
+            {
+                Y = -Height; //top so above top
+            }
+            else
+            {
+                Y = gameWindow.Height; //below so below bottom
+            }
+        }
+        else
+        {
+            //left or right
+            Y = SplashKit.Rnd(gameWindow.Height);
+
+            if (SplashKit.Rnd() < 0.5)
+            {
+                X = -Width;
+            }
+            else
+            {
+                X = gameWindow.Width;
+            }
+        }
+
+        // set the speed and assign to the velocity
+        Velocity = SplashKit.VectorMultiply(dir, speed);
+    }
+
+    public void Update()
+    {
+        X += Velocity.X;
+        Y += Velocity.Y;
+    }
+
+    public bool IsOffscreen(Window screen)
+    {
+        if (X < -Width || X > screen.Width || Y < -Height || Y > screen.Height)
+        {
+            return true;
+        }
+        return false;
     }
 
     public void Draw()
--- RobotDodgee.cs
+++ RobotDodgee.cs
@@ -1,4 +1,5 @@
 using SplashKitSDK;
+using System.Collections.Generic;
 #nullable disable
 
 public class RobotDodgee
@@ -6,14 +7,14 @@ public class RobotDodgee
 
     private Player player;
     private Window gameWindow;
-    private Robot testRobot;
+    private List<Robot> _Robots;
     public bool Quit { get { return player.Quit; } }
 
     public RobotDodgee(Window gameWindow)
     {
         this.gameWindow = gameWindow;
         player = new Player(gameWindow);
-        testRobot = RandomRobot();
+        _Robots = new List<Robot>();
     }
 
     public void HandleInput()
@@ -25,19 +26,55 @@ public class RobotDodgee
     public void Draw()
     {
         gameWindow.Clear(Color.Beige);
-        testRobot.Draw();
+
+        foreach (Robot robot in _Robots)
+        {
+            robot.Draw();
+        }
+
         player.Draw();
         gameWindow.Refresh(60);
     }
 
-    public void Update() { 
-        if(player.CollidedWith(testRobot)) {
-            testRobot = RandomRobot();
+    public void Update()
+    {
+        foreach (Robot robot in _Robots)
+        {
+            robot.Update();
         }
+
+        if (_Robots.Count >= 0)
+        {
+            _Robots.Add(RandomRobot());
+        }
+
+        CheckCollisions();
+
     }
 
-    public Robot RandomRobot() {
-        return new Robot(gameWindow);
+    private void CheckCollisions()
+    {
+        List<Robot> toBeRemovedRobots = new List<Robot>();
+
+        foreach (Robot robot in _Robots)
+        {
+            if (player.CollidedWith(robot) || robot.IsOffscreen(gameWindow))
+            {
+                toBeRemovedRobots.Add(robot);
+            }
+
+        }
+
+        foreach (Robot bot in toBeRemovedRobots)
+        {
+            _Robots.Remove(bot);
+        }
+
+    }
+
+    public Robot RandomRobot()
+    {
+        return new Robot(gameWindow, player);
     }
 
 }
